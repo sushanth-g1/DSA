8f964a311929a9dd57278dbd34bf6febc448bae7
sushanth-g1 <sush18.sushanth@gmail.com>
2024-01-12 21:35:12 +0530

    majority element DSA

---
+++ src/com/dsa/learning/MajorityElement.java
@@ -0,0 +1,25 @@
+package com.dsa.learning;
+
+public class MajorityElement {
+    public static int majorityElement(int[] nums) {
+        int element = Integer.MIN_VALUE;
+        int count = 0;
+
+        for(int i=0;i<nums.length;i++){
+            if(count == 0){
+                count = 1;
+                element = nums[i];
+            }else if(element == nums[i]){
+                count++;
+            }else{
+                count --;
+            }
+        }
+        return element;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums));
+    }
+}
