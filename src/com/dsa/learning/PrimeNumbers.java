956eae5c0a2e513713fec7ba1bfa478b76413a95
sushanth-g1 <sush18.sushanth@gmail.com>
2024-01-12 15:07:53 +0530

    DSA-Prime_No's

---
+++ src/com/dsa/learning/PrimeNumbers.java
@@ -0,0 +1,66 @@
+package com.dsa.learning;
+
+public class PrimeNumbers {
+    //Prime Numbers
+    public static boolean checkPrime(int n){
+        if(n<=1){
+            return false;
+        }
+        int noOfFactor = 0;
+        for(int i=1; i<=n;i++){
+            if(n%i == 0){
+                noOfFactor++;
+            }
+            System.out.println(" **** i : "+i);
+        }
+        if(noOfFactor == 2){
+            return true;
+        }
+        return false;
+    }
+    public static boolean checkPrime2(int n){
+        if(n<=1){
+            return false;
+        }
+        int noOfFactor = 0;
+        for(int i=1; i<=Math.sqrt(n);i++){
+            if(n%i == 0){
+                noOfFactor++;
+            }
+            System.out.println(" **** i : "+i);
+        }
+        if(noOfFactor == 2){
+            return true;
+        }
+        return false;
+    }
+    public static void seivePrime(int n){
+        boolean[] prime = new boolean[n+1];
+        for(int i=2;i<=n;i++){                      //for(int i=2;i<=Math.sqrt(n);i++)  i<=Math.sqrt(n) -----> 2nd Optimisation
+            if(prime[i] == false){
+                for (int j=i*2;j<=n;j=j+i){         //for (int j=i*i;j<=n;j=j+i)  j=i*i -----> 1st Optimisation
+                    prime[j] = true;
+                }
+            }
+        }
+        for (int i=2;i<=n;i++){
+            if (prime[i] == false){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+//        System.out.println(checkPrime(24));
+//        System.out.println(checkPrime2(24));
+
+        seivePrime(24);
+
+        long endTime = System.currentTimeMillis();
+        long runtime = endTime - startTime;
+        System.out.println("Runtime: " + runtime + " milliseconds");
+
+    }
+}
