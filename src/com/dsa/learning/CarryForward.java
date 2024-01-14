8837832fbbe8e035445d4a604ce6278c5fdfc26b
sushanth-g1 <sush18.sushanth@gmail.com>
2024-01-14 18:00:12 +0530

    prefix_carryForward

---
+++ src/com/dsa/learning/CarryForward.java
@@ -0,0 +1,56 @@
+package com.dsa.learning;
+
+public class CarryForward {
+    public static int subArray(char[] s){
+        int c = 0,n=s.length;
+        int ans = 0;
+        for(int i=n-1;i>=0;i--){
+            if(s[i] == 'g')
+                c++;
+            else if(s[i] == 'a')
+                ans += c;
+        }
+        return ans;
+    }
+    public static int maxMin(int[] arr){
+        int max = arr[0];
+        for(int maximum : arr){
+            max = Math.max(max,maximum);
+        }
+        int min = arr[0];
+        for(int minimum : arr){
+            min = Math.min(min,minimum);
+        }
+
+
+
+        int n=arr.length,len=0, ans = n, minIndex = -1, maxIndex = -1;
+        for(int i=n-1;i>0;i--){
+            if(min == max)
+                return 1;
+            if(arr[i]==min){
+                minIndex = i;
+                if(maxIndex != -1){
+                    len = maxIndex-minIndex;
+                    ans = Math.min(len,ans);
+                }
+            }else if(arr[i]==max){
+                maxIndex = i;
+                if(minIndex != -1){
+                    len = minIndex-maxIndex;
+                    ans = Math.min(len,ans);
+                }
+            }
+        }
+
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+//        char[] s = {'a','d','g','a','g','a','g','f','g'};
+//        System.out.println(subArray(s));
+        int[] ints = {1,6,4,6,5,1,5,2,6,4,4};
+        System.out.println(maxMin(ints));
+    }
+}
---
+++ src/com/dsa/learning/PrefixSum.java
@@ -0,0 +1,94 @@
+package com.dsa.learning;
+
+public class PrefixSum {
+    public static int[] sum(int[] arr){
+        int n = arr.length;
+        int[] Pf = new int[n];
+        Pf[0] = arr[0];
+        for(int i=1;i<n;i++){
+            Pf[i]=Pf[i-1]+arr[i];                    // TC : O(N)
+        }
+
+        // Print Prefix Sum
+        for(int j=0; j<n;j++){
+            System.out.println(Pf[j]);
+        }
+        return Pf;
+    }
+
+    // Equilibrium Index
+    public static void equilibriumIndex(int[] arr,int[] prefixSum){
+        int c = 0;
+        int n = prefixSum.length, left = 0, right = 0;
+        for (int i=0; i<n; i++){
+            if(i==0){
+                left = 0;
+            }
+            else {
+                left = prefixSum[i-1];
+            }
+            right = prefixSum[n-1] - prefixSum[i];
+            if (left == right){
+                System.out.println(" ********** "+arr[i]);
+                System.out.println(" ********** "+left+"  ********** "+right);
+                c++;
+            }
+        }
+    }
+
+    public static int[] prefixEven(int[] arr){
+        int n = arr.length;
+        int[] prefixEven = new int[n];
+        prefixEven[0] = arr[0];
+        for(int i=1;i<n;i++){
+            if(i%2 == 0){
+                prefixEven[i] = prefixEven[i-1]+arr[i];
+            }
+            else{
+                prefixEven[i] = prefixEven[i-1];
+            }
+        }
+        return prefixEven;
+    }
+    public static int[] prefixOdd(int[] arr){
+        int n = arr.length;
+        int[] prefixOdd = new int[n];
+        prefixOdd[0] = 0;
+        for(int i=1;i<n;i++){
+            if(i%2 != 0){
+                prefixOdd[i] = prefixOdd[i-1]+arr[i];
+            }
+            else{
+                prefixOdd[i] = prefixOdd[i-1];
+            }
+        }
+        return prefixOdd;
+    }
+    public static void specialIndex(int[] arr, int[] prefixEven, int[] prefixOdd){
+        int c = 0,n = arr.length,sumEven=0,sumOdd=0;
+        for(int i=0;i<n;i++){
+            if(i==0){
+                sumEven = prefixOdd[n-1]-prefixOdd[i];
+                sumOdd  = prefixEven[n-1]-prefixEven[i];
+            }else {
+                sumEven = prefixEven[i-1]+(prefixOdd[n-1]-prefixOdd[i]);
+                sumOdd = prefixOdd[i-1]+(prefixEven[n-1]-prefixEven[i]);
+            }
+            if(sumEven == sumOdd){
+                System.out.println("*** index : "+i+", element : "+arr[i]+", sumEven = "+sumEven+", sumOdd = "+sumOdd);
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] arr ={4,3,2,7,6,-2};
+//        sum(arr);
+//        equilibriumIndex(arr,sum(arr));
+        specialIndex(arr,prefixEven(arr),prefixOdd(arr));
+
+    }
+}
